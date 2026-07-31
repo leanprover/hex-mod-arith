e32dbcb49f4e91e6ba357f1235ecb2b0c81e4753
Kim Morrison <kim@leanprover.com>
2026-07-31 11:11:45 +0000

    chore: bootstrap released repository

---
+++ lakefile.lean
@@ -0,0 +1,25 @@
+import Lake
+
+open System Lake DSL
+
+package «hex-mod-arith» where
+  leanOptions := #[⟨`doc.verso, true⟩, ⟨`doc.verso.suggestions, false⟩]
+
+require HexArith from git
+  "https://github.com/leanprover/hex-arith.git" @ "9d1eeb75bdf9d9f26a97c5ab7d5aec371876c587"
+
+private def zmod64MulOTarget (pkg : Package) : FetchM (Job FilePath) := do
+  let oFile := pkg.dir / defaultBuildDir / "HexModArith" / "ffi" / "zmod64_mul.o"
+  let srcTarget ← inputTextFile <| pkg.dir / "HexModArith" / "ffi" / "zmod64_mul.c"
+  buildFileAfterDep oFile srcTarget fun srcFile => do
+    compileO oFile srcFile #["-I", (← getLeanIncludeDir).toString, "-fPIC", "-O3"]
+
+extern_lib hexmodarithffi (pkg) := do
+  let name := nameToStaticLib "hexmodarithffi"
+  let oTarget ← zmod64MulOTarget pkg
+  buildStaticLib (pkg.staticLibDir / name) #[oTarget]
+
+@[default_target]
+lean_lib HexModArith where
+  precompileModules := true
+  moreLinkArgs := #["-lgmp"]
